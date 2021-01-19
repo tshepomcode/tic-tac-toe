5a1dd61359b2cb8945c8e2e58147f2e9c997349e
tshepomcode <tshepomcode@gmail.com>
2021-01-19 05:42:48 +0200

    possible win selection testing

--- selection.rb
+++ selection.rb
@@ -1,6 +1,21 @@
 require 'pry'
 
 # binding.pry
+def is_winner(board)
+
+  selection = get_selection(board)
+  winner = []
+
+  win_1 = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+  win_2 = [[1, 4, 7], [2, 5, 8], [3, 6, 9]]
+  win_3 = [[1, 5, 9], [3, 5, 7]]
+
+  win_arr = [win_1, win_2, win_3]
+
+  win_arr.each_with_index { |item, index|
+  item.include?(selection) ? winner.push(true) : winner.push(false)
+  }
+end
 
 
 b1_arr = ["X", "O", "X", "O", "X", 6, 7, 8, 9]
@@ -28,4 +43,12 @@ test_x = 'X'
 test_o = 'O'
 
 # binding.pry
-(t1_arr.count(test_x) > 2) ? (puts "Yes!") : (puts "No!")
+if (t1_arr.count(test_x) > 2) 
+  puts "Yes!"
+else 
+  puts "No!"
+end
+
+# are the X's at location [[1, 2, 3], [4, 5, 6], [7, 8, 9]]?
+# are the X's at location [[1, 4, 7], [2, 5, 8], [3, 6, 9]]?
+# are the X's at location [[1, 5, 9], [3, 5, 7]]?
